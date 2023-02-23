2bd04d6c4c7db347e3daf2ede838e1352c170c46
chloeadriancreates <chloeadriancreates@gmail.com>
2023-02-23 18:18:37 +0100

    Setup ESLint

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,9 @@
+import "./Profile.scss";
+
+function Profile() {
+    return (
+        <div></div>
+    );
+}
+
+export default Profile;
