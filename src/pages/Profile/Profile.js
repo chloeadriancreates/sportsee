6087e316748985b8a5ed7c0d2c9dfb4100ab4fc7
chloeadriancreates <chloeadriancreates@gmail.com>
2023-02-26 12:01:02 +0100

    Update Profile with user data

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -24,17 +24,19 @@ function Profile() {
             <Header />
             <div className="profile-main">
                 <Sidebar />
-                <div className="profile-main-content">
-                    <Greeting name="Thomas" />
-                    <div className="profile-main-content-stats">
-                        <div className="profile-main-content-stats-counters">
-                            <Counter value="1,930" type="Calories" />
-                            <Counter value="155" type="Protéines" />
-                            <Counter value="290" type="Glucides" />
-                            <Counter value="50" type="Lipides" />
+                { user &&
+                    <div className="profile-main-content">
+                        <Greeting name={user.data.userInfos.firstName} />
+                        <div className="profile-main-content-stats">
+                            <div className="profile-main-content-stats-counters">
+                                <Counter value={user.data.keyData.calorieCount} type="Calories" />
+                                <Counter value={user.data.keyData.proteinCount} type="Protéines" />
+                                <Counter value={user.data.keyData.carbohydrateCount} type="Glucides" />
+                                <Counter value={user.data.keyData.lipidCount} type="Lipides" />
+                            </div>
                         </div>
                     </div>
-                </div>
+                }
             </div>
         </div>
     );
