a9bb8237c1168c50cab575ed95b14eb6ea2360ed
chloeadriancreates <chloeadriancreates@gmail.com>
2023-02-26 11:18:28 +0100

    Add link to homepage in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import "./Header.scss";
 
 function Header() {
     return (
         <header className="header">
-            <img src={logo} alt="Logo de Sportsee" />
+            <Link to="/">
+                <img src={logo} alt="Logo de Sportsee" />
+            </Link>
             <p className="header-link">Accueil</p>
             <p className="header-link">Profil</p>
             <p className="header-link">Réglages</p>
