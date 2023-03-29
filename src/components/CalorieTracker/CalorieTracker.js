2ee7ffd6c347e9c27e0a17030b501594b3dafde5
chloeadriancreates <chloeadriancreates@gmail.com>
2023-03-29 09:38:05 +0200

    Fix fonts on CalorieTracker

--- src/components/CalorieTracker/CalorieTracker.js
+++ src/components/CalorieTracker/CalorieTracker.js
@@ -13,7 +13,16 @@ function CalorieTracker({ data }) {
             barGap={8}
             barCategoryGap={40}
         >
-            <XAxis dataKey="name" tickLine={false} />
+            <XAxis
+                dataKey="name"
+                tickLine={false}
+                style={{
+                    fontSize: "14px",
+                    fontFamily: "Roboto",
+                    fontWeight: "500",
+                    color: "#74798C"
+                }}
+            />
             <YAxis dataKey="calories" hide={true} />
             <YAxis
                 yAxisId="weight"
@@ -24,19 +33,18 @@ function CalorieTracker({ data }) {
                 axisLine={false}
                 tickMargin={20}
                 tickCount={3}
+                style={{
+                    fontSize: "14px",
+                    fontFamily: "Roboto",
+                    fontWeight: "500",
+                    color: "#74798C"
+                }}
             />
             <CartesianGrid vertical={false} strokeDasharray="2" />
             <Tooltip content={CalorieTooltip} />
             <Bar dataKey="weight" fill="#282D30" radius={[50, 50, 0, 0]} name="Poids (kg)" />
             <Bar dataKey="calories" fill="#E60000" radius={[50, 50, 0, 0]} name="Calories (kCal)" />
             <Legend content={CalorieLegend} verticalAlign="top" />
-            {/* <Legend
-                verticalAlign="top"
-                align="right"
-                iconType="circle"
-                iconSize={7}
-                height={80}
-            /> */}
         </BarChart>
     );
 }
