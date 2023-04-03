36e233d7f163ca796eef238ef0f26c977581e621
chloeadriancreates <chloeadriancreates@gmail.com>
2023-04-03 19:07:21 +0200

    Add status to UserCard

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -4,10 +4,12 @@ import user from "../../assets/user.png";
 import "./UserCard.scss";
 
 function UserCard({ id, name }) {
+    const online = id === "12" || id === "18";
     return (
         <Link to={`/user/${id}`} className="userCard">
             <img src={user} alt="Utilisateur" className="userCard-icon" />
             <p className="userCard-name">{name}</p>
+            <p className="userCard-status">{online ? "Connecté" : "Déconnecté"}</p>
         </Link>
     );
 }
